f268c9c20c2f81e5c22d387260a615ecc94737ea
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-10 18:16:05 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+
+
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of sphere
+ */
+function convertingTemperature() {
+  // input
+  const temperatureInFahrenheit = parseFloat(document.getElementById('temperature-in-fahrenheit').value)
+  // process
+  const temperatureInCelcius = (temperatureInFahrenheit - 32) * 5.0/9.0
+  // output
+  document.getElementById('answer').innerHTML = "The temperature is: " + temperatureInCelcius.toFixed(3) + " °C"
+}
